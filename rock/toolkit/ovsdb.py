4b29b93f68109ba7e5add4d478a1c2bc9f8cbaee
kd.shi <kd.shi@slancer.com>
2018-08-20 22:30:13 +0800

    add delete remote mac method

--- rock/toolkit/ovsdb.py
+++ rock/toolkit/ovsdb.py
@@ -9,10 +9,12 @@ OVSDB_PORT = 6641
 DEFAULT_DB = 'hardware_vtep'
 BUFFER_SIZE = 4096
 
+
 class OvsdbClient(object):
     """
 
     """
+
     def __init__(self):
         self._client = None
 
@@ -39,11 +41,24 @@ class OvsdbClient(object):
                         [])
 
     def del_ucast_macs_remote(self, uuid=None):
+        """
+        :param uuid:
+        :return:
+        """
         if uuid:
-            operations = {
-
-            }
-            self.run_operat(operations)
+            method = "transact"
+            params = ["hardware_vtep",
+                      {
+                          "op": "delete",
+                          "table": "Ucast_Macs_Remote",
+                          "where": [["_uuid", "==", uuid]]
+                      },
+                      {
+                          "durable": True,
+                          "op": "commit"
+                      }
+                      ]
+            self.run_operat(method, params)
 
 
 def get_client():
@@ -51,5 +66,9 @@ def get_client():
     client.connect((OVSDB_IP, OVSDB_PORT))
     return client
 
+
 ovsdb_client = OvsdbClient()
 ovsdb_client.list_databases()
+
+uuid = '1c2b0140-1478-4a25-a3a6-92b7b89fc60c'
+ovsdb_client.del_ucast_macs_remote(uuid)
